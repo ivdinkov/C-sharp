1408d21409c4de3c8067be7e8c82fb9dd370c823
Ivan Dinkov <ivdinkov@gmail.com>
2017-10-12 22:42:40 +0100

    Palindrome

---
+++ PalindromeWithUnitTest/Pallindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "navan";
+
+            PalindromeChecker pch = new PalindromeChecker();
+
+            if (pch.IsPallindrome(s))
+            {
+                Console.WriteLine("It is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not palindrome");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}

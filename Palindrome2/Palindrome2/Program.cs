c4dd127a7630f400cefd7524e6ed38aab707f437
Ivan Dinkov <ivdinkov@gmail.com>
2017-10-17 14:19:11 +0100

    Palindrome with for loop

---
+++ Palindrome2/Palindrome2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (IsPalindrome("navan"))
+            {
+                Console.WriteLine("It is");
+            }
+            else
+            {
+                Console.WriteLine("it is not");
+            }
+            Console.ReadLine();
+        }
+
+        private static bool IsPalindrome(string v)
+        {
+            char[] a = v.ToCharArray();
+            int l = a.Length - 1;
+
+            for (int i = 0; i < l; i++)
+            {
+                if (a[i] != a[l-i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}

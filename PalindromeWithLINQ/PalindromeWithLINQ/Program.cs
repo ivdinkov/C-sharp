0b99472149fd4666baa07211c12e29df1f818fdf
Ivan Dinkov <ivdinkov@gmail.com>
2017-10-17 14:12:26 +0100

    Palindrome with LINQ

---
+++ PalindromeWithLINQ/PalindromeWithLINQ/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeWithLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "navan";
+
+            if (IsPalindrome(word))
+            {
+                Console.WriteLine("it is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("it is not");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static bool IsPalindrome(string word)
+        {
+            char[] a = word.ToCharArray();
+            string n = "";
+
+            foreach (var t in Enumerable.Reverse(a.ToList()))
+            {
+                n += t;
+            }
+
+            return n.Equals(word);
+        }
+    }
+}

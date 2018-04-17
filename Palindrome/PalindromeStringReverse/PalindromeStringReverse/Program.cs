78ef18ddc43150e1a7e5eff74aaf00b8e46305f1
Ivan Dinkov <ivdinkov@gmail.com>
2018-04-17 10:55:37 +0100

    palindrome exercises added

---
+++ Palindrome/PalindromeStringReverse/PalindromeStringReverse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeStringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (IsPalindrome("navan"))
+            {
+                Console.WriteLine("It is");
+            }
+            else
+            {
+                Console.WriteLine("it is not");
+            }
+            Console.ReadLine();
+        }
+
+        private static bool IsPalindrome(string v)
+        {
+            string x = "";
+            for (int i = v.Length - 1; i >= 0; i--)
+            {
+                x += v[i];
+            }
+            return v == x;
+        }
+    }
+}
